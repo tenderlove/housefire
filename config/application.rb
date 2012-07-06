7c00df25322b1ac00e7280bba22211a84c1c9a67
Aaron Patterson <aaron.patterson@gmail.com>
2012-07-06 16:43:35 -0700

    hopefully this fixes heroku

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module Housefire
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.assets.initialize_on_precompile = false
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types.
