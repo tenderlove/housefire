1df1d86007b42fa7d980a4a371ef31652565c53d
Aaron Patterson <aaron.patterson@gmail.com>
2012-07-06 14:32:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Housefire::Application.config.secret_token = '3aadb375c19e85fa703dbdf1006f3262360e3d7e9b84c5cfb1503ebc09f15053028dcbc00647da4369ff1c4e46427d155a4405a08ff27d78ced394b65b151581'
